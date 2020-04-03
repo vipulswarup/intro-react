1f8a6143268f9adf4e5701edc16d72e04caf7a6a
Vipul Swarup <vipulswarup@Vipuls-MacBook-Air.local>
2020-04-03 14:46:24 +0530

    interim

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-}
+const Pet = props => {
+  const { name, animal, breed, media, location, id } = props;
+
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </a>
+  );
+};
+
+export default Pet;
